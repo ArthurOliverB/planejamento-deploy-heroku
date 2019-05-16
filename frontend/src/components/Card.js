0520e85d50c669d4c577d9d911e1b0d5de9a353f
Arthur Oliveira <arthuroliveirabandeira@hotmail.com>
2019-05-16 16:59:23 -0300

    evol

--- frontend/src/components/Card.js
+++ frontend/src/components/Card.js
@@ -2,20 +2,32 @@ import React from 'react';
 import animate from 'animate.css'
 import './Card.css';
 import plus from '../assets/plus.png'
+
 import axios from 'axios'
 class Card extends React.Component {
-
+    
+    
 
     constructor() {
         super()
+        
         this.state = {
-            name: 'Nova tarefa', 
-            note: 'Teste', 
+            name: '',
+            note: '', 
             prio: ''
         }
         this.handleNameChange = this.handleNameChange.bind(this)
         this.handleNoteChange = this.handleNoteChange.bind(this)
     }
+
+   componentWillMount() {
+       this.setState({
+            name: this.props.name,
+            note: this.props.note, 
+            prio: this.props.status
+       })
+       
+   }
 
     handleNameChange(e) {
         this.setState({
@@ -31,19 +43,29 @@ class Card extends React.Component {
     renderEditable() {
         
         return (
-            <div className="card-container">
-                <div className="card editable">
-                    <div className="tag editable event" onClick={e => this.changeClass(e)}></div>
-                    <div className="content">
-                        <input className="input title" value={this.state.name} onChange={this.handleNameChange} />
-                        <input className="input subtitle" value={this.state.note} onChange={this.handleNoteChange}/>
+            <div className="static">
+                <div className="card-container editable">
+                    <div className="tag event editable" onClick={e => this.changeClass(e)}></div>
+                    <div className="card-info">
+                        <div className="header-wrapper editable">
+                            <div className="card-header editable">
+                        <input className="input title" placeholder = "Nova tarefa" value={this.state.name} onChange={this.handleNameChange} />                                
+                            </div>
+                        </div>
+                        <div className="content-wrapper editable">
+                            <div className="card-content editable">
+                        <input className="input subtitle" placeholder = "Notas" value={this.state.note} onChange={this.handleNoteChange}/>
+                                
+                            </div>
+                        </div>
                     </div>
-                </div>
-                
-                <button onClick={e => this.animateButton(e)}>
-                    <img src={plus} alt="add"/>
-                </button>
+                    <div className="card-actions">
+                    <button className="done" onClick={e => this.animateButton(e)}>
+                    </button>
+                       
+                    </div>
             </div>
+            </div> 
         )
     }
     
@@ -60,11 +82,22 @@ class Card extends React.Component {
 
         if(this.state.prio!== '' && this.state.name!== '' && this.state.note!=='') {
             this.postNewTask()
+            this.reset(e)
         } else {
             alert("Preencha todos os campos")
         }
     }
+    reset(e) {
+        let element = document.querySelectorAll("div[class^=tag]")[0]
+        
+        element.className = 'tag editable event' 
 
+        this.setState({
+            name: '',
+            note: '',
+            prio: ''
+        })
+    }
     postNewTask() {
                 
         axios.post('http://localhost:4000/tasks', {
@@ -77,61 +110,99 @@ class Card extends React.Component {
     }
 
     changeClass(e) {
+
+
         let element = e.target
 
-        let className = element.className.split(' ')[2]
+        let className = element.className.split(' ')[1]
         let elementClass = ''
 
         
-        switch(className) {
-            case 'success':
-                elementClass = 'warning'
-                this.setState({
-                    prio: 'm'
-                })
-                break
-            case 'warning': 
-                elementClass= 'failure'
-                this.setState({
-                    prio: 'h'
-                })
-                break
-            case 'failure': 
-                elementClass = 'success'
-                this.setState({
-                    prio: 'b'
-                })
-                break
-            default:
-                elementClass = 'success'
-                this.setState({
-                    prio: 'b'
-                })
-                break
+        if(element.className.split(' ')[2] === 'editable') {
+            switch(className) {
+                case 'success':
+                    elementClass = 'warning'
+                    this.setState({
+                        prio: 'm'
+                    })
+                    break
+                case 'warning': 
+                    elementClass= 'failure'
+                    this.setState({
+                        prio: 'h'
+                    })
+                    break
+                case 'failure': 
+                    elementClass = 'success'
+                    this.setState({
+                        prio: 'b'
+                    })
+                    break
+                default:
+                    elementClass = 'success'
+                    this.setState({
+                        prio: 'b'
+                    })
+                    break
+            }
+            element.className = 'tag ' + elementClass + ' editable'
         }
-        element.className = 'tag editable ' + elementClass
     }
 
     renderStatic() {
         let status = ''
 
-        if (this.props.status === 'h') {
+        if (this.state.prio === 'h') {
             status = 'failure'
-        } else if (this.props.status === 'm') {
+        } else if (this.state.prio === 'm') {
             status = 'warning'
         } else {
             status = 'success'
         }
 
         return (
-                <div className="card" >
-                    <div className={'tag ' + status}></div>
-                    <div className="content">
-                        <p className="title">{this.props.name}</p>
-                        <p className="subtitle">{this.props.note}</p>
+            <div className="card-container">
+                <div className={'tag ' + status} onClick={e => this.changeClass(e)}> </div>
+                <div className="card-info">
+                    <div className="header-wrapper">
+                        <div className="card-header">
+                        <input className="dynamic input title" placeholder = "Nova tarefa" value={this.state.name} onChange={this.handleNameChange} readOnly/>
+                        </div>
+                    </div>
+                    <div className="content-wrapper">
+                        <div className="card-content">
+                            <input className=" dynamic input subtitle" placeholder = "Notas" value={this.state.note} onChange={this.handleNoteChange} readOnly/>
+                        </div>
                     </div>
                 </div>
+                <div className="card-actions">
+                    <button className="done"></button>
+                    <button className="edit" onClick={ e => this.makeEditable(e)}></button>
+                    <button className="delete"></button>
+                </div>
+            </div>
         )
+    }
+
+    makeEditable(e) {
+        let parentDiv = e.target.parentNode.parentNode
+        let className = parentDiv.classList[1]
+        let title = parentDiv.children[1].children[0].children[0].children[0]
+        let note = parentDiv.children[1].children[1].children[0].children[0]
+        let tag = parentDiv.children[0]
+
+        if(className) {
+            parentDiv.className = 'card-container'
+            title.readOnly = true
+            note.readOnly = true
+            // VERIFICAR
+            tag.className.split(' ')[2] = ''           
+        } else {
+            parentDiv.className = 'card-container editable'
+            title.readOnly = false
+            note.readOnly = false
+            tag.className += ' editable'
+        }
     }
     render() {
         
